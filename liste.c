5f0eb5ad0585a0181410ba993dce7c5fbc3071de
wejdene <wbwejdene@gmail.com>
2023-12-31 22:15:29 +0100

    modification du fichier liste c

--- liste.c
+++ liste.c
@@ -39,12 +39,11 @@ liste supprimer(liste l) {
 
 void afficherliste(liste l) {
     liste tmp = l;
-    do {
+     while (tmp->next != NULL) {
         affichage_voie(tmp->contenu);
         tmp=tmp->next;
     }
-    while (tmp->next != NULL);
-}
+    }
 
 int case_vide(liste l) {
     return (int)(l->next==NULL);
